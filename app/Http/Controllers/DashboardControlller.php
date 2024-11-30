50814100694d7a17ca79abd773dd2d92526b4285
joaovsj <joaosilva.jesus90@gmail.com>
2024-11-30 20:44:23 -0300

    :sparkles: feat: methods to get the quantity and the price of movementes done

--- app/Http/Controllers/DashboardControlller.php
+++ app/Http/Controllers/DashboardControlller.php
@@ -8,7 +8,29 @@ use Illuminate\Support\Facades\DB;
 
 class DashboardControlller extends Controller
 {
-    public function bar()
+    private $status   = true;
+    private $quantity = [];
+    private $sum      = [];
+
+    public function index()
+    {
+        
+        try {
+            $this->quantityByCategory();
+            $this->sumByCategory();
+
+        } catch (\Throwable $th) {
+            $status = false;        
+        }
+
+        return response()->json([
+            'status'    => $this->status,
+            "quantity"  => $this->quantity,
+            "sum"       => $this->sum
+        ]);
+    }
+
+    public function quantityByCategory()
     {
         $total = DB::table('categories')
             ->join('products', 'categories.id', '=', 'products.category_id')
@@ -16,8 +38,7 @@ class DashboardControlller extends Controller
             ->select('categories.name as category_name', DB::raw('SUM(movements.quantity) as total_quantity'))
             ->groupBy('categories.name')
             ->get();
-        
-        
+          
         $labels = [];
         $amount = [];
 
@@ -26,14 +47,36 @@ class DashboardControlller extends Controller
             array_push($labels, $value->category_name);
             array_push($amount, intval($value->total_quantity));
         }
+    
+        $this->quantity = [ 
+            'labels' => $labels,
+            'amount' => $amount
+        ];
+    }
 
-        return response()->json([
-            'status' => true,
-            'body'   => [
-                'labels' => $labels,
-                'amount' => $amount
-            ]
-        ]);
+    public function sumByCategory()
+    {
+        $total = DB::table('categories')
+            ->join('products', 'categories.id', '=', 'products.category_id')
+            ->join('movements', 'products.id', '=', 'movements.product_id')
+            ->select('categories.name as category_name', DB::raw('SUM(movements.price) as price'))
+            ->groupBy('categories.name')
+            ->get();
+        
+        
+        $labels = [];
+        $sum    = [];
 
+        foreach ($total as $key => $value) {
+        
+            array_push($labels, $value->category_name);
+            array_push($sum,    round($value->price, 2));
+        }
+
+
+        $this->sum = [ 
+            'labels' => $labels,
+            'sum'    => $sum  
+        ];
     }
 }
